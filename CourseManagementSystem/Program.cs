fece4ddbd160bd55ca7c1a1a0c2dd1bf1b930243
AkeelAriskhan <akeelariskhan1314@gmail.com>
2024-10-10 17:43:52 +0530

    progam cs input function

--- CourseManagementSystem/Program.cs
+++ CourseManagementSystem/Program.cs
@@ -10,6 +10,71 @@ namespace CourseManagementSystem
     {
         static void Main(string[] args)
         {
+            var coursemanager=new CourseManager();  
+            bool exit=true;
+            while (exit) 
+            {
+               Console.Clear();
+                Console.WriteLine("Course Management System:");
+                Console.WriteLine("1. Add a Course:");
+                Console.WriteLine("2. View All Courses");
+                Console.WriteLine("3. Update a Course");
+                Console.WriteLine("4. Delete a Course");
+                Console.WriteLine("5. Exit");
+                Console.Write("Choose an option:");
+                var selectnum=Convert.ToInt32(Console.ReadLine());
+
+                switch (selectnum) 
+                {
+                  case 1:
+                        Console.Clear();
+                        Console.Write("Enter a Course Title");
+                        var title=Console.ReadLine();
+                        Console.Write("Enter a course Duration");
+                        var Duration=Console.ReadLine();
+                        Console.Write("Enter a course Price");
+                        var Price=decimal.Parse(Console.ReadLine());
+                        coursemanager.CreateCourse(title, Duration, Price);
+                        break;
+                 case 2:
+                        Console.Clear();
+                        coursemanager.ReadCourses();
+                        break;
+                 case 3:
+                        Console.Clear();
+                        Console.Write("Enter a  Course CourseId");
+                        var CourseId1=Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Enter a Course Title");
+                        var title1 = Console.ReadLine();
+                        Console.Write("Enter a course Duration");
+                        var Duration1 = Console.ReadLine();
+                        Console.Write("Enter a course Price");
+                        var Price1 = decimal.Parse(Console.ReadLine());
+                        coursemanager.UpdateCourse(CourseId1,title1,Duration1, Price1);
+                        break;
+                 case 4:
+                        Console.Clear();
+                        Console.Write("Enter a  Course CourseId");
+                        var CourseId2 = Convert.ToInt32(Console.ReadLine());
+                        coursemanager.DeleteCourse(CourseId2);
+                        break;
+                  case 5:
+                        exit=false;
+                        return;
+                   default:
+                        Console.WriteLine("Invalid number try again");
+                        break;
+                }
+
+
+
+
+                Console.WriteLine("Enter Any Key continue");
+                Console.ReadKey();
+
+
+            }
+
         }
     }
 }
